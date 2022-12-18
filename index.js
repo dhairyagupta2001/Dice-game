de73b26bed98bd03ea219848de880d54b4ea6d6f
dhairyagupta2001 <119852152+dhairyagupta2001@users.noreply.github.com>
2022-12-18 15:26:40 +0530

    add files in this

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.random();
+randomNumber1=randomNumber1*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+var randomNumber2=Math.random();
+randomNumber2=randomNumber2*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="💖Player1 wins";
+}else
+if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player2 wins💖";
+}else{
+    document.querySelector("h1").innerHTML="TIEEEE😅";
+}
